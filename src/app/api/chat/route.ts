b4144dba868eceab7203ceea72115aa3b1d4103e
Deepu <d4udts@gmail.com>
2025-03-04 04:59:17 +0100

    [temp] add gmail & calender

--- src/app/api/chat/route.ts
+++ src/app/api/chat/route.ts
@@ -6,12 +6,19 @@ import { Calculator } from '@langchain/community/tools/calculator';
 import { SerpAPI } from '@langchain/community/tools/serpapi';
 import { SystemMessage } from '@langchain/core/messages';
 
+import { GmailSearch } from '@/lib/tools/gmail/search';
+import { getGoogleAccessToken } from '@/lib/auth0';
 import { convertVercelMessageToLangChainMessage } from '@/utils/message-converters';
 import { logToolCallsInDevelopment } from '@/utils/stream-logging';
+import { GmailCreateDraft } from '@/lib/tools/gmail/create_draft';
+import { GmailGetMessage } from '@/lib/tools/gmail/get_message';
+import { GmailGetThread } from '@/lib/tools/gmail/get_thread';
+import { GmailSendMessage } from '@/lib/tools/gmail/send_message';
+import { GoogleCalendarCreateTool, GoogleCalendarViewTool } from '@/lib/tools/google_calendar';
 
-export const runtime = 'edge';
+export const runtime = 'nodejs';
 
-const AGENT_SYSTEM_TEMPLATE = `You are a personal assistant named Assistant0. You are a helpful assistant that can answer questions and help with tasks. You have access to a set of tools, use the tools as needed to answer the user's question.`;
+const AGENT_SYSTEM_TEMPLATE = `You are a personal assistant named Assistant0. You are a helpful assistant that can answer questions and help with tasks. You have access to a set of tools, use the tools as needed to answer the user's question. Render the email body as a markdown block, do not wrap it in code blocks.`;
 
 /**
  * This handler initializes and calls an tool calling ReAct agent.
@@ -30,13 +37,38 @@ export async function POST(req: NextRequest) {
       .filter((message: Message) => message.role === 'user' || message.role === 'assistant')
       .map(convertVercelMessageToLangChainMessage);
 
-    // Requires process.env.SERPAPI_API_KEY to be set: https://serpapi.com/
-    const tools = [new Calculator(), new SerpAPI()];
     const chat = new ChatOpenAI({
-      model: 'gpt-4',
+      model: 'gpt-4o-mini',
       temperature: 0,
     });
 
+    // Provide the access token to the Gmail tools
+    const gmailParams = {
+      credentials: {
+        accessToken: await getGoogleAccessToken(),
+      },
+    };
+
+    const googleCalendarParams = {
+      credentials: {
+        accessToken: await getGoogleAccessToken(),
+        calendarId: 'primary',
+      },
+      model: chat,
+    };
+
+    const tools = [
+      new Calculator(),
+      // Requires process.env.SERPAPI_API_KEY to be set: https://serpapi.com/
+      new SerpAPI(),
+      new GmailSearch(gmailParams),
+      new GmailCreateDraft(gmailParams),
+      new GmailGetMessage(gmailParams),
+      new GmailGetThread(gmailParams),
+      new GmailSendMessage(gmailParams),
+      new GoogleCalendarCreateTool(googleCalendarParams),
+      new GoogleCalendarViewTool(googleCalendarParams),
+    ];
     /**
      * Use a prebuilt LangGraph agent.
      */
--- src/lib/auth0.ts
+++ src/lib/auth0.ts
@@ -1,3 +1,18 @@
 import { Auth0Client } from '@auth0/nextjs-auth0/server';
 
-export const auth0 = new Auth0Client();
+export const auth0 = new Auth0Client({
+  // this is required to get federated access tokens from services like Google
+  authorizationParameters: {
+    access_type: 'offline',
+    prompt: 'consent',
+  },
+});
+
+export const getGoogleAccessToken = async () => {
+  const { token } = await auth0.getAccessTokenForConnection({
+    connection: 'google-oauth2',
+  });
+  return token;
+
+  //   return process.env.GMAIL_ACCESS_TOKEN;
+};
